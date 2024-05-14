9ed242b7b6b41c75784e6a9b69ab95175870ff00
kisandras <160039853+kisandras@users.noreply.github.com>
2024-05-14 20:44:26 +0200

    Add files via upload
    Végső verzió

--- Szamologep/Szamologep/MainWindow.xaml.cs
+++ Szamologep/Szamologep/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ namespace CalculatorApp
 {
     public partial class MainWindow : Window
     {
+        //Kis András készítette
         private string jelenlegiSzam = "";
         private string elozoSzam = "";
         private string muvelet = "";
@@ -14,7 +15,7 @@ namespace CalculatorApp
         public MainWindow()
         {
             InitializeComponent();
-            this.KeyDown += new System.Windows.Input.KeyEventHandler(MainWindow_KeyDown);
+            this.KeyDown += new System.Windows.Input.KeyEventHandler(FoAblak_BillentyuLenyomas);
         }
 
         private void SzamGomb_Click(object sender, RoutedEventArgs e)
@@ -44,8 +45,16 @@ namespace CalculatorApp
             {
                 if (jelenlegiSzam != "")
                 {
-                    elozoSzam = jelenlegiSzam;
-                    jelenlegiSzam = "";
+                    if (!string.IsNullOrEmpty(jelenlegiSzam))
+                    {
+                        if (!string.IsNullOrEmpty(elozoSzam))
+                        {
+                            EredmenyKiszamitas();
+                        }
+                        elozoSzam = jelenlegiSzam;
+                        jelenlegiSzam = "";
+                    }
+                    
                     muvelet = button.Content.ToString();
                 }
             }
@@ -109,71 +118,71 @@ namespace CalculatorApp
             muvelet = "";
             Kijelzo.Text = "0";
         }
-        
-        private void MainWindow_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        //Tóth Kende készítette
+        private void FoAblak_BillentyuLenyomas(object sender, System.Windows.Input.KeyEventArgs e)
         {
             switch (e.Key)
             {
-                // SZámok gombjai
+                // Számok gombjai
                 case System.Windows.Input.Key.D0:
                 case System.Windows.Input.Key.NumPad0:
-                    AppendNumber("0");
+                    SzamMellekeles("0");
                     break;
                 case System.Windows.Input.Key.D1:
                 case System.Windows.Input.Key.NumPad1:
-                    AppendNumber("1");
+                    SzamMellekeles("1");
                     break;
                 case System.Windows.Input.Key.D2:
                 case System.Windows.Input.Key.NumPad2:
-                    AppendNumber("2");
+                    SzamMellekeles("2");
                     break;
                 case System.Windows.Input.Key.D3:
                 case System.Windows.Input.Key.NumPad3:
-                    AppendNumber("3");
+                    SzamMellekeles("3");
                     break;
                 case System.Windows.Input.Key.D4:
                 case System.Windows.Input.Key.NumPad4:
-                    AppendNumber("4");
+                    SzamMellekeles("4");
                     break;
                 case System.Windows.Input.Key.D5:
                 case System.Windows.Input.Key.NumPad5:
-                    AppendNumber("5");
+                    SzamMellekeles("5");
                     break;
                 case System.Windows.Input.Key.D6:
                 case System.Windows.Input.Key.NumPad6:
-                    AppendNumber("6");
+                    SzamMellekeles("6");
                     break;
                 case System.Windows.Input.Key.D7:
                 case System.Windows.Input.Key.NumPad7:
-                    AppendNumber("7");
+                    SzamMellekeles("7");
                     break;
                 case System.Windows.Input.Key.D8:
                 case System.Windows.Input.Key.NumPad8:
-                    AppendNumber("8");
+                    SzamMellekeles("8");
                     break;
                 case System.Windows.Input.Key.D9:
                 case System.Windows.Input.Key.NumPad9:
-                    AppendNumber("9");
+                    SzamMellekeles("9");
                     break;
                 case System.Windows.Input.Key.OemPeriod:
                 case System.Windows.Input.Key.Decimal:
-                    AppendNumber(".");
+                    SzamMellekeles(".");
                     break;
 
                 // Műveleti gombok
                 case System.Windows.Input.Key.Add:
                 case System.Windows.Input.Key.OemPlus when Keyboard.Modifiers == ModifierKeys.None:
-                    Setmuvelet("+");
+                    SetMuvelet("+");
                     break;
                 case System.Windows.Input.Key.Subtract:
                 case System.Windows.Input.Key.OemMinus:
-                    Setmuvelet("-");
+                    SetMuvelet("-");
                     break;
                 case System.Windows.Input.Key.Multiply:
-                    Setmuvelet("*");
+                    SetMuvelet("*");
                     break;
                 case System.Windows.Input.Key.Divide:
-                    Setmuvelet("/");
+                    SetMuvelet("/");
                     break;
 
                 // Egyenlőség gomb
@@ -184,11 +193,12 @@ namespace CalculatorApp
                 // Törlő gombok
                 case System.Windows.Input.Key.Back:
                 case System.Windows.Input.Key.Delete:
-                    ClearCalculator();
+                    SzamologepTorlese();
                     break;
             }
         }
-        private void AppendNumber(string szam)
+        //Kis András készítette
+        private void SzamMellekeles(string szam)
         {
             if (szam == ".")
             {
@@ -204,14 +214,23 @@ namespace CalculatorApp
             Kijelzo.Text = jelenlegiSzam;
         }
 
-        private void Setmuvelet(string op)
+        private void SetMuvelet(string op)
         {
-            if (jelenlegiSzam != "")
+            if (!string.IsNullOrEmpty(jelenlegiSzam))
             {
-                elozoSzam = jelenlegiSzam;
-                jelenlegiSzam = "";
-                muvelet = op;
+                if (!string.IsNullOrEmpty(elozoSzam))
+                {
+                    EredmenyKiszamitas();
+                    elozoSzam = jelenlegiSzam;
+                    jelenlegiSzam = "";
+                }
+                else
+                {
+                    elozoSzam = jelenlegiSzam;
+                    jelenlegiSzam = "";
+                }
             }
+            muvelet = op;
         }
 
         private void EredmenyKiszamitas()
@@ -264,7 +283,7 @@ namespace CalculatorApp
             }
         }
 
-        private void ClearCalculator()
+        private void SzamologepTorlese()
         {
             jelenlegiSzam = "";
             elozoSzam = "";
